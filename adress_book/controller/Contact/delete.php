a3a950fc62fda6de6af91d2146c4a61c1dd0a631
rokkit <fss92@pochta.ru>
2012-12-24 16:58:22 +0400

    Comm

---
+++ adress_book/controller/Contact/delete.php
@@ -0,0 +1,5 @@
+﻿<?php
+    $link = new mysqli("http://mysql-env-6698993.jelastic.servint.net","root","EssKAYcLPa","adress_book");
+    $id=$_GET['id'];//из гет запроса забираем номер записи
+    if($link->query("DELETE FROM Contact WHERE id=$id"));
+?>
